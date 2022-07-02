d6c5e8c6246dcdb348920a3a76f675bb82fe9485
Ballistc-dot <pedrocdn2014@gmail.com>
2022-07-02 04:40:52 -0300

    finnaly resolving the problem of isCompleted i think

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -15,17 +15,14 @@ interface Todos {
   isCompleted: boolean
 }
 export function Todo({ onDelete, content, onChecked, id }: TodoProps) {
-  const todos: Todos[] | undefined = useSelector((state: any) => state.todo)
+  const todos: Todos[] = useSelector((state: any) => state.todo)
 
   const storageTodo = todos?.filter((todo: Todos) => todo?.id === id)
 
-  const [isChecked, setIsChecked] = useState(
-    storageTodo && storageTodo![0].isCompleted
-  )
+  if (!storageTodo[0]) return
 
   function handleCheck() {
-    setIsChecked(!isChecked)
-    onChecked(id, !isChecked)
+    onChecked(id, !storageTodo[0].isCompleted)
   }
   function handleDelete() {
     onDelete(id)
@@ -40,6 +37,7 @@ export function Todo({ onDelete, content, onChecked, id }: TodoProps) {
     exit: { scale: 0, opacity: 0, delay: 0.2 },
     trasition: { stiffness: 200, damping: 40, delay: 0.2 },
   }
+  const storageCompleted = storageTodo[0].isCompleted
   return (
     <motion.li
       {...animations}
@@ -49,10 +47,10 @@ export function Todo({ onDelete, content, onChecked, id }: TodoProps) {
         <input
           className="bg-transparent ring-2 rounded-lg  ring-blue-300 checked:bg-purple-600 checked:ring-purple-600 hover:bg-blue-400 hover:ring-blue-400 checked:hover:bg-purple-400 checked:hover:ring-purple-400 cursor-pointer"
           type="checkbox"
-          checked={isChecked}
+          checked={storageTodo[0].isCompleted}
           onChange={handleCheck}
         />
-        {isChecked ? (
+        {storageCompleted ? (
           <del className="text-gray-300 text-sm">{content}</del>
         ) : (
           <p className="text-gray-100 text-sm">{content}</p>
